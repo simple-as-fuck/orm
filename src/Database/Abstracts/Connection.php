730b299d1b4c26f4464f813fabaef4a47c0cf977
Votech Brozek <triplkrypl@centrum.cz>
2020-07-26 19:28:44 +0200

    add php stan and fix some errors from stan

--- src/Database/Abstracts/Connection.php
+++ src/Database/Abstracts/Connection.php
@@ -8,6 +8,8 @@ abstract class Connection
 {
     /**
      * send one query into database and return result, which can be fetch
+     *
+     * @param string[]|int[]|float[] $inputParameters
      */
     abstract public function query(string $statement, array $inputParameters): Result;
 }
