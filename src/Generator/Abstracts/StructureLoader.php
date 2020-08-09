f93ea5ec3b98c02c6d574b56b2bb5a863992ffcb
Votech Brozek <triplkrypl@centrum.cz>
2020-08-09 19:19:50 +0200

    add some checks into abstract models structure loading

--- src/Generator/Abstracts/StructureLoader.php
+++ src/Generator/Abstracts/StructureLoader.php
@@ -11,5 +11,46 @@ abstract class StructureLoader
      *
      * @return ModelStructure[]
      */
-    abstract public function loadModels(): array;
+    public function loadModels(): array
+    {
+        $models = $this->loadStructure();
+        $checkedModels = [];
+
+        foreach ($models as $model) {
+            if (count($model->getPrimaryKeys()) === 0) {
+                throw new \LogicException('For model: "'.$model->getName().'" are primaryKeys empty. All models must have one or more property as primary (unique) key!');
+            }
+
+            $notAssignableKeys = 0;
+            foreach ($model->getPrimaryKeys() as $primaryKey) {
+                if (! $primaryKey->isAssignable()) {
+                    $notAssignableKeys++;
+                }
+            }
+
+            if ($notAssignableKeys > 1) {
+                throw new \LogicException('Model: "'.$model->getName().'" has to many not assignable primaryKeys. Maximum of loaded primary keys from persist layer is one!');
+            }
+
+            $simpleParams = $model->getSimpleParams();
+            usort($simpleParams, function (ModelProperty $a, ModelProperty $b): int {
+                if ($a->getDefaultValue() === null && $b->getDefaultValue() !== null) {
+                    return 1;
+                }
+
+                return -1;
+            });
+
+            $checkedModels[] = new ModelStructure($model->getName(), $model->getComment(), $model->getPrimaryKeys(), $model->getAdditionalKeys(), $simpleParams);
+        }
+
+        return $checkedModels;
+    }
+
+    /**
+     * method load structure from some source
+     *
+     * @return ModelStructure[]
+     */
+    abstract protected function loadStructure(): array;
 }
