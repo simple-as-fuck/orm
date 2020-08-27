10b86f11c2154b19b02c2d3caeda320119e6793a
Votech Brozek <triplkrypl@centrum.cz>
2020-08-27 19:01:33 +0200

    config change abstract method and add types check

--- src/Config/Abstracts/Config.php
+++ src/Config/Abstracts/Config.php
@@ -6,5 +6,46 @@ namespace SimpleAsFuck\Orm\Config\Abstracts;
 
 abstract class Config
 {
-    abstract public function getString(string $key): string;
+    final public function getString(string $key): string
+    {
+        $value = $this->getValue($key);
+        if (! is_string($value)) {
+            throw new \RuntimeException('Config key: "'.$key.'" is not string');
+        }
+
+        return $value;
+    }
+
+    /**
+     * @return string[]
+     */
+    final public function getArrayOfString(string $key): array
+    {
+        $values = $this->getArray($key);
+        foreach ($values as $value) {
+            if (! is_string($value)) {
+                throw new \RuntimeException('Config key: "'.$key.'" is not array of string');
+            }
+        }
+
+        return $values;
+    }
+
+    /**
+     * @return mixed
+     */
+    abstract protected function getValue(string $key);
+
+    /**
+     * @return mixed[]
+     */
+    final private function getArray(string $key): array
+    {
+        $value = $this->getValue($key);
+        if (! is_array($value)) {
+            throw new \RuntimeException('Config key: "'.$key.'" is not array');
+        }
+
+        return $value;
+    }
 }
