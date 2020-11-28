bde7cd5b7945de060e812ba38139b324f170e1d4
Votech Brozek <triplkrypl@centrum.cz>
2020-11-28 20:30:14 +0100

    change database structure for integration test

--- test/Integration/Generator/TestMysqlTables.php
+++ test/Integration/Generator/TestMysqlTables.php
@@ -14,29 +14,31 @@ $connection->query('
         `int` int not null,
         `string` varchar(255) not null,
         createdAt datetime not null,
+        timeAt datetime default null,
         deletedAt varchar(255) default null,
-        `float` float default 0.0,
-        primary key (id)
+        `float` float not null default 0.0,
+        primary key (id),
+        index timeAtIndex (timeAt)
+    )
+', []);
+
+$connection->query('
+    create table TestMap(
+        someId int unsigned not null ,
+        anotherId int unsigned not null ,
+        primary key (someId, anotherId) 
     )
 ', []);
 
 $connection->query("
-    create table TestMap(
-        someId int unsigned not null ,
-        anotherId int unsigned not null ,
-        deletedAt varchar(255) default null,
-        `value` decimal(10,2) default '0.0',
-        primary key (someId, anotherId) 
-    )
-", []);
-
-$connection->query('
     create table TestMultiplePrimaryKey(
-        someId int unsigned not null,
+        id int unsigned not null auto_increment,
         `date` date not null,
         customType varchar(255) not null,
         anotherId int unsigned default null,
-        primary key (someId, date),
+        deletedAt varchar(255) default null,
+        `value` decimal(10,2) not null default '0.0',
+        primary key (id, date),
         index anotherIdIndex (anotherId)
     )
-', []);
+", []);
